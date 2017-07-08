ef8f0db3a9d7fe786599d9b33999f413c3ceb9a5
George Heming <gheming@gmail.com>
2017-07-08 18:32:55 -0400

    Initial Commit

---
+++ Ideal-Octo-Invention/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ideal-Octo-Invention
+//
+//  Created by George Heming on 7/8/17.
+//  Copyright © 2017 George Heming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ideal-Octo-InventionTests/Ideal_Octo_InventionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ideal_Octo_InventionTests.swift
+//  Ideal-Octo-InventionTests
+//
+//  Created by George Heming on 7/8/17.
+//  Copyright © 2017 George Heming. All rights reserved.
+//
+
+import XCTest
+@testable import Ideal_Octo_Invention
+
+class Ideal_Octo_InventionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
